961a5a7debf2c88c312f356e72da06e2fc33291f
Bakhuya Milka Wanga <bakhuyamilka@gmail.com>
2023-04-07 12:09:52 -0700

    send emails

--- Lipa_na_mpesa.php
+++ Lipa_na_mpesa.php
@@ -222,6 +222,8 @@ if ((in_array($_SERVER['REMOTE_ADDR'], $whitelist))) {
 
 
   // send email
+
+  send_email($email_address='',$message='');
   
    }
   $_SESSION['merchant_request_id'] =$merchantRequestID; 
@@ -234,6 +236,72 @@ file_put_contents('mpesa_payment.txt',date('d M Y H:i:s')." ".$_SERVER['REMOTE_A
 
 echo json_encode(array("rescode" =>76,"developer_message"=>"fake payment record", "resmsg" => "Your IP address  has been blocked."));
 }    
+}
+
+
+function send_email($email_address='',$message=''){
+
+
+//  initialize post fields
+                $post_fieds = json_encode(array(
+                    
+                        "From"=> "geofrey.ongidi@digitalvision.co.ke",
+                        "To"=> $email_address,
+                        "Subject"=> "Hello from Postmark",
+                        "HtmlBody"=> $message,
+                        "MessageStream"=> "notifications"
+                    
+                ));
+
+                // if get token
+                if($token = get_authorization_token()){
+                    $url = "https://api.postmarkapp.com/email";
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => $url,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS =>$post_fieds,
+                    CURLOPT_HTTPHEADER => array(
+                        "Authorization: Bearer".$token,
+                        "Content-Type: application/json",
+                        "Accept: application/json",
+                        "X-Postmark-Server-Token: b1371069-335f-431b-8f45-88c22d7f1c47"
+                    ),
+                    ));
+                    $response = curl_exec($curl);
+                    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                    $err = curl_error($curl);
+                    curl_close($curl);
+                    if ($err) {
+                        
+                        return FALSE;
+                    } else {                    
+                        if($response){
+                            if($file = json_decode($response)){ 
+
+                                //store data in db
+                                
+                                print_r($file);
+                               
+                            }else{
+                                return FALSE;
+                            }
+                            //print_r($file);die;
+                        }else{
+                            $error = $err?:'';
+                            $code = $httpcode?:'';
+                            return FALSE;
+                        }
+                    }
+                }else{
+                    return FALSE;
+                }      
 }
 
 ?>
