7c944960c35b66314fa8875f79bfa1904e7b7868
Bakhuya Milka Wanga <bakhuyamilka@gmail.com>
2023-04-07 11:08:18 -0700

    implement stk push processing

--- Lipa_na_mpesa.php
+++ Lipa_na_mpesa.php
@@ -1,6 +1,10 @@
 <?php
 
-function get_authorization_token($base_url,$consumer_key,$consumer_secret){
+function get_authorization_token(){
+
+    $base_url=env("BASE_URL");
+    $consumer_key=env("CONSUMER_KEY");
+    $consumer_secret=env("CONSUMER_SECRET");
     
     $encoded_key=base64_encode($consumer_key.":".$consumer_secret);
     $curl = curl_init();
@@ -32,5 +36,75 @@ function get_authorization_token($base_url,$consumer_key,$consumer_secret){
         }
     }
 }
+
+
+
+function mpesa_stk_push($BusinessShortCode='',$Password='',$Timestamp=0,$TransactionType='',$Amount='',$PartyA='',$PartyB='',$PhoneNumber='',$CallBackURL='',$AccountReference='',$TransactionDesc=''){
+        
+                $post_fieds = json_encode(array(
+                        "BusinessShortCode"=>$BusinessShortCode,
+                        "Password"=>$Password,
+                        "Timestamp"=> $Timestamp,
+                        "TransactionType"=> $TransactionType,
+                        "Amount"=> $Amount,
+                        "PartyA"=> $PartyA,
+                        "PartyB"=> $PartyB,
+                        "PhoneNumber"=> $PhoneNumber,
+                        "CallBackURL"=> "https://www.paxcoins.com/callback.php",
+                        "AccountReference"=> $AccountReference,
+                        "TransactionDesc"=> $TransactionDesc 
+                    
+                   
+                ));
+                if($token = $this->get_authorization_token()){
+                    $url = env("BASE_URL")."/mpesa/stkpush/v1/processrequest";
+                    $curl = curl_init();
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => $url,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS =>$post_fieds,
+                    CURLOPT_HTTPHEADER => array(
+                        "Authorization: Bearer".$token,
+                        "Content-Type: application/json"
+                    ),
+                    ));
+                    $response = curl_exec($curl);
+                    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+                    $err = curl_error($curl);
+                    curl_close($curl);
+                    if ($err) {
+                        
+                        return FALSE;
+                    } else {                    
+                        if($response){
+                            if($file = json_decode($response)){ 
+
+                                //store data in db
+                                
+                                print_r($file);
+                               
+                            }else{
+                                return FALSE;
+                            }
+                            //print_r($file);die;
+                        }else{
+                            $error = $err?:'';
+                            $code = $httpcode?:'';
+                            return FALSE;
+                        }
+                    }
+                }else{
+                    return FALSE;
+                }
+            
+        
+}
+
 
 ?>
